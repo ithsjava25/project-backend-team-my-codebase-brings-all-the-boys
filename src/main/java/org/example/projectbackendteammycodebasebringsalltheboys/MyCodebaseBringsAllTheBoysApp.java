d15ea62f4b7b7e16ee795a964f49754a1166c41d
Mats Rönnqvist <mats.f.ronnqvist@gmail.com>
2026-03-26 10:10:33 +0100

    first commit

---
+++ src/main/java/org/example/projectbackendteammycodebasebringsalltheboys/MyCodebaseBringsAllTheBoysApp.java
@@ -0,0 +1,13 @@
+package org.example.projectbackendteammycodebasebringsalltheboys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCodebaseBringsAllTheBoysApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCodebaseBringsAllTheBoysApp.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projectbackendteammycodebasebringsalltheboys/MyCodebaseBringsAllTheBoysAppTests.java
@@ -0,0 +1,13 @@
+package org.example.projectbackendteammycodebasebringsalltheboys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCodebaseBringsAllTheBoysAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
